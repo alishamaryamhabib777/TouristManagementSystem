066ce389f1bfc40cae71a9db6378492f5ac14c39
Alisha Maryam Habib <alishamaryamhabib7@gmail.com>
2026-05-01 00:36:10 +0500

    Add initial tms.cpp file with includes and PI definition

---
+++ tms.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <cmath>
+#include <string>
+#include <iomanip>
+#include <fstream>
+#include <algorithm>
+#include <limits> 
+
+using namespace std;
+
+#ifndef M_PI
+// Define PI if it's not already defined by the math library
+#define M_PI 3.14159265358979323846
+#endif
